3164858d08f453b412d90294078ff9270b05f138
SamunBista1 <quantityquantity5@gmail.com>
2026-03-26 15:48:14 +0100

    Delete ExpenserTracker directory

--- ExpenserTracker/src/main/java/com/example/expensetracker/repository/ExpenseRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.expensetracker.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.expensetracker.model.Expense;
-
-public interface ExpenseRepository extends JpaRepository<Expense, Long> {
-}
